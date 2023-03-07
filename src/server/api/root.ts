a60425d502c94744770ac348b49b4d2fde6832e5
Cédric Bazureau <cedric.bazureau@renault.com>
2023-03-07 21:35:47 +0100

    init schema

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { communeRouter } from "~/server/api/routers/commune";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  commune: communeRouter,
 });
 
 // export type definition of API
--- src/server/api/routers/commune.ts
+++ src/server/api/routers/commune.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
 
-export const exampleRouter = createTRPCRouter({
+export const communeRouter = createTRPCRouter({
   hello: publicProcedure
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
@@ -10,7 +10,7 @@ export const exampleRouter = createTRPCRouter({
         greeting: `Hello ${input.text}`,
       };
     }),
-  getAll: publicProcedure.query(({ ctx }) => {
-    return ctx.prisma.example.findMany();
+  getCommunes: publicProcedure.query(({ ctx }) => {
+    return ctx.prisma.commune.findMany();
   }),
 });
